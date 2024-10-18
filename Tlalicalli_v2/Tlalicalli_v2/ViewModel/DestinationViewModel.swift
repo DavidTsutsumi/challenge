1ab7b43f1931c9ccec2b5bde1c76fc843ab922a1
Facultad de Contaduría y Administración <facultaddecontaduriayadministracion@MacBook-33.local>
2024-10-18 13:13:40 -0600

    mas etiquetas

--- Tlalicalli_v2/Tlalicalli_v2/ViewModel/DestinationViewModel.swift
+++ Tlalicalli_v2/Tlalicalli_v2/ViewModel/DestinationViewModel.swift
@@ -15,6 +15,10 @@ class DestinationViewModel: ObservableObject {
     @Published var destinations: [Destination] = [
         Destination(name: "Centro de Reciclaje Milpa Alta", location: "Milpa Alta, CDMX", imageName: "recycle", category: "PET", latitude: 19.3005, longitude: -99.1043),
         Destination(name: "Punto Verde", location: "Milpa Alta, CDMX", imageName: "recycle", category: "Electrónicos", latitude: 19.3050, longitude: -99.1070),
+        Destination(name: "Centro de Reciclaje Milpa Alta", location: "Milpa Alta, CDMX", imageName: "recycle", category: "PET", latitude: 19.3105, longitude: -99.1040),
+        Destination(name: "Centro de Reciclaje Milpa Alta", location: "Milpa Alta, CDMX", imageName: "recycle", category: "PET", latitude: 19.3205, longitude: -99.1040),
+        Destination(name: "Centro de Reciclaje Milpa Alta", location: "Milpa Alta, CDMX", imageName: "recycle", category: "PET", latitude: 19.3305, longitude: -99.1049),
+
         // Agrega más destinos según sea necesario
     ]
 }
